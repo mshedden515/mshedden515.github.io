312f70b70403ef85f35742989d0913f2375e65f7
Lucas Guerrero <guerrerolucas246@gmail.com>
2022-06-10 16:48:25 -0300

    mas ajustes generales

---
+++ TP2/js/script.js
@@ -0,0 +1,13 @@
+(function() {
+
+  'use strict';
+
+  function fakeLoading(){
+      
+  }
+
+  document.getElementById('searchgames').addEventListener(onclick, function(){
+  document.getElementById('searchresults').classList.remove('d-none');
+  })
+
+})();
